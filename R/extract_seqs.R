a158d69bfce891badf84701275f44e6c157a353f
marine-ecologist <george.roff@csiro.au>
2023-08-30 06:57:27 +1000

    minor edits

--- R/extract_seqs.R
+++ R/extract_seqs.R
@@ -8,10 +8,10 @@
 #' @export
 #' @return A data.frame of seq.ID (columns) and sample.ID (rows) with either relative or absolute abundance of sequences.
 #' @examples
-#' ## not run:
-#' # tmp <- extract_seqs(folder="/Users/rof011/symbiodinium/20220919T102058_esampayo", type="absolute", clade=c("C", "D"))
+#'
+#' #tmp <- extract_seqs(folder="/Users/rof011/symbiodinium/20220919T102058_esampayo", type="absolute", clade=c("C", "D"))
 #
-#' # tmp2 <- extract_seqs(folder="/Users/rof011/symbiodinium/20220919T102058_esampayo", type="relative", clade="C", cutoff=100)
+#' #tmp2 <- extract_seqs(folder="/Users/rof011/symbiodinium/20220919T102058_esampayo", type="relative", clade="C", cutoff=100)
 
 
 extract_seqs <- function(folder, type = "relative", clade = LETTERS[1:10], cutoff=1000, silent=TRUE) {
@@ -21,8 +21,8 @@ extract_seqs <- function(folder, type = "relative", clade = LETTERS[1:10], cutof
   absolute <- read.delim(paste0(folder, "/", file_list)) %>%
     dplyr::select(sample_name, 40:ncol(.)) %>% # select just the symbiodinium columns
     tibble::column_to_rownames("sample_name") %>% # sample_name column to rowname
-    dplyr::slice(-n()) %>% # remove the last row, summary data
-    dplyr::filter(rowSums(select(., where(is.numeric))) > as.numeric(cutoff)) %>% # remove samples where <1000 sequences
+    dplyr::slice(-dplyr::n()) %>% # remove the last row, summary data
+    dplyr::filter(rowSums(dplyr::select(., dplyr::where(is.numeric))) > as.numeric(cutoff)) %>% # remove samples where <1000 sequences
     dplyr::select(dplyr::matches(clade, ignore.case = FALSE)) %>% # keep only columns matching "clade"
     dplyr::filter(rowSums(dplyr::select(., dplyr::where(is.numeric))) != 0) %>% # drop zero sum rows
     dplyr::select(dplyr::where(~ sum(. != 0) > 0)) %>% # drop zero sum columns
@@ -31,9 +31,9 @@ extract_seqs <- function(folder, type = "relative", clade = LETTERS[1:10], cutof
   excluded_samples <- read.delim(paste0(folder, "/", file_list)) %>%
     dplyr::select(sample_name, 40:ncol(.)) %>% # select just the symbiodinium columns
     tibble::column_to_rownames("sample_name") %>% # sample_name column to rowname
-    dplyr::slice(-n()) %>% # remove the last row, summary data
+    dplyr::slice(-dplyr::n()) %>% # remove the last row, summary data
     dplyr::select(dplyr::matches(clade, ignore.case = FALSE)) %>% # keep only columns matching "clade"
-    dplyr::filter(rowSums(select(., where(is.numeric))) < as.numeric(cutoff)) # remove samples where <1000 sequences
+    dplyr::filter(rowSums(dplyr::select(., dplyr::where(is.numeric))) < as.numeric(cutoff)) # remove samples where <1000 sequences
 
   if (silent==FALSE){
     cat("Excluded samples \n")
@@ -43,14 +43,14 @@ extract_seqs <- function(folder, type = "relative", clade = LETTERS[1:10], cutof
   relative <- read.delim(paste0(folder, "/", file_list)) %>%
     dplyr::select(sample_name, 40:ncol(.)) %>% # select just the symbiodinium columns
     tibble::column_to_rownames("sample_name") %>% # sample_name column to rowname
-    dplyr::slice(-n()) %>% # remove the last row, summary data
-    dplyr::filter(rowSums(select(., where(is.numeric))) > as.numeric(cutoff)) %>% # remove samples where <1000 sequences
+    dplyr::slice(-dplyr::n()) %>% # remove the last row, summary data
+    dplyr::filter(rowSums(dplyr::select(., dplyr::where(is.numeric))) > as.numeric(cutoff)) %>% # remove samples where <1000 sequences
     dplyr::select(dplyr::matches(clade, ignore.case = FALSE)) %>% # keep only columns matching "clade"
     dplyr::filter(rowSums(dplyr::select(., dplyr::where(is.numeric))) != 0) %>% # drop zero sum rows
     dplyr::select(dplyr::where(~ sum(. != 0) > 0)) %>% # drop zero sum columns
     dplyr::select(dplyr::where(~ any(!is.na(.)))) %>% # drop blank columns
-    dplyr::mutate(row_sum = rowSums(select(., dplyr::where(is.numeric)))) %>%
-    dplyr::mutate(across(dplyr::where(is.numeric), ~ . / row_sum)) %>%
+    dplyr::mutate(row_sum = rowSums(dplyr::select(., dplyr::where(is.numeric)))) %>%
+    dplyr::mutate(dplyr::across(dplyr::where(is.numeric), ~ . / row_sum)) %>%
     dplyr::select(-row_sum)
 
   if (type == "absolute") {
