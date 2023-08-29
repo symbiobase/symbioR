43fc3bf452ae52815f7fe5061da5fc9332076ef4
marine-ecologist <george.roff@csiro.au>
2023-08-30 06:57:27 +1000

    minor edits

--- R/extract_its2.R
+++ R/extract_its2.R
@@ -4,14 +4,13 @@
 #' @export
 #' @return its_profile_UID A data.frame of ITS profiles for each UID
 #' @examples
-#'
 #' tmp <- extract_its2(folder="/Users/rof011/Symbiodinium/20220919T102058_esampayo/")
 
 extract_its2 <- function(folder) {
 
-    a <- extract_its2_profile(folder=folder)
-    b <- extract_its2_profile_UID(folder=folder)
-    its_profile <- left_join(a, b, by = "sample.ID") %>%
+    a <- symportalfunctions::extract_its2_profile(folder = folder)
+    b <- symportalfunctions::extract_its2_profile_UID(folder = folder)
+    its_profile <- dplyr::left_join(a, b, by = "sample.ID") |>
       as.data.frame()
 
 
